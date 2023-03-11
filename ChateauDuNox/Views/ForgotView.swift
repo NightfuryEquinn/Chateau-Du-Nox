762f7d94d37b548f9cf8f4b6bcf027233e23f67a
YIP ZI XIAN <tp059963@TL05-FSEC-07.local>
2023-03-11 13:09:00 +0800

    Save progress

---
+++ ChateauDuNox/Views/ForgotView.swift
@@ -0,0 +1,20 @@
+//
+//  ForgotView.swift
+//  ChateauDuNox
+//
+//  Created by YIP ZI XIAN on 11/03/2023.
+//
+
+import SwiftUI
+
+struct ForgotView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ForgotView_Previews: PreviewProvider {
+    static var previews: some View {
+        ForgotView()
+    }
+}
--- ChateauDuNox/Views/LoginView.swift
+++ ChateauDuNox/Views/LoginView.swift
@@ -8,8 +8,14 @@
 import SwiftUI
 
 struct LoginView: View {
+    // Custom Colors
     let cBlack = Color(red: 0.14, green: 0.17, blue: 0.20)
     let cYellow = Color(red: 238.0/255.0, green: 235.0/255.0, blue: 209.0/255.0)
+    let cDarkGreen = Color(red: 68/255, green: 88/255, blue: 39/255)
+    let cLightGreen = Color(red: 103/255, green: 132/255, blue: 56/255)
+    
+    // Boolean for View Switching
+    @State private var showSignupView = false
     
     @State private var username = ""
     @State private var password = ""
@@ -27,9 +33,65 @@ struct LoginView: View {
                 
                 
                 Text("Chateau Du Nox")
-                    .font(Font.custom("Vollkorn-Regular", size: 24))
+                    .font(.custom("Vollkorn-Regular", size: 40))
                     .foregroundColor(cBlack)
                     .padding(.bottom, 40)
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    Text("Username")
+                        .font(.headline)
+                        .foregroundColor(cBlack)
+                    
+                    TextField("Enter username", text: $username)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    
+                    Text("Password")
+                        .font(.headline)
+                        .foregroundColor(cBlack)
+                    
+                    SecureField("Enter password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                }
+                .padding(.horizontal, 40)
+                .padding(.bottom, 40)
+                
+                Button(action: {
+                    print("Login")
+                }) {
+                    Text("Login")
+                        .font(.headline)
+                        .foregroundColor(cYellow)
+                        .padding(16)
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(cDarkGreen)
+                        .cornerRadius(50)
+                }
+                .padding(.horizontal, 120)
+                .padding(.bottom, 40)
+                
+                HStack {
+                    Button(action: {
+                        self.showSignupView = true
+                    }) {
+                        Text("Register")
+                            .font(.subheadline)
+                            .foregroundColor(cBlack)
+                    }
+                    .sheet(isPresented: $showSignupView) {
+                        SignupView()
+                    }
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        print("To Forgot Password")
+                    }) {
+                        Text("Forgot Password")
+                            .font(.subheadline)
+                            .foregroundColor(cBlack)
+                    }
+                }
+                .padding(.horizontal, 80)
             }
             .background(cYellow)
         }
