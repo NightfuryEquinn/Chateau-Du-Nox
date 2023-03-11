9de1313788374d2edd0ac3261ad6cd6bae82dbfa
Yip (Equinn) Zi Xian <86152710+NightfuryEquinn@users.noreply.github.com>
2023-03-11 10:24:19 +0800

    Add files via upload

---
+++ ChateauDuNox/ChateauDuNoxApp.swift
@@ -0,0 +1,20 @@
+//
+//  ChateauDuNoxApp.swift
+//  ChateauDuNox
+//
+//  Created by YIP ZI XIAN on 11/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ChateauDuNoxApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
